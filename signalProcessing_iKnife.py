d5bbfd9fe53e4d653f59823875391c8e00fe107e
JoshEhrlich <60014815+JoshEhrlich@users.noreply.github.com>
2020-03-31 15:56:59 -0400

    added simplified data and new version of code

--- signalProcessing_iKnife.py
+++ signalProcessing_iKnife.py
@@ -17,34 +17,34 @@ Y_test = np.empty((200,1))
 
 for i in range(int(0.05*len(Y_train))):
 
-	rand_int1 = random.randint(0,int(0.25*len(X_train))-i)
+	rand_int1 = random.randint(0,int(0.25*len(X_train))-i-1)
 	X_test[i,0,:] = X_train[rand_int1][0]
 	X_test[i,1,:] = X_train[rand_int1][1]
 	Y_test[i,:] = Y_train[rand_int1]
 	X_train = np.delete(X_train, rand_int1, 0)
 	Y_train = np.delete(Y_train, rand_int1, 0)
 
-	rand_int2 = random.randint(int(0.25*len(X_train))-i, int(0.5*len(X_train))-i)
+	rand_int2 = random.randint(int(0.25*len(X_train))-i, int(0.5*len(X_train))-i-1)
 	X_test[i+50,0,:] = X_train[rand_int2][0]
 	X_test[i+50,1,:] = X_train[rand_int2][1]
 	Y_test[i+50,:] = Y_train[rand_int2]
 	X_train = np.delete(X_train, rand_int2, 0)
 	Y_train = np.delete(Y_train, rand_int2, 0)
 
-	rand_int3 = random.randint(int(0.5*len(X_train))-i, int(0.75*len(X_train))-i)
+	rand_int3 = random.randint(int(0.5*len(X_train))-i, int(0.75*len(X_train))-i-1)
 	X_test[i+100,0,:] = X_train[rand_int3][0]
 	X_test[i+100,1,:] = X_train[rand_int3][1]
 	Y_test[i+100,:] = Y_train[rand_int3]
 	X_train = np.delete(X_train, rand_int3, 0)
 	Y_train = np.delete(Y_train, rand_int3, 0)
 
-	rand_int4 = random.randint(int(0.75*len(X_train))-i, len(X_train)-i)
+	rand_int4 = random.randint(int(0.75*len(X_train))-i, len(X_train)-i-1)
 	X_test[i+150,0,:] = X_train[rand_int3][0]
 	X_test[i+150,1,:] = X_train[rand_int3][1]
 	Y_test[i+150,:] = Y_train[rand_int4]
 	X_train = np.delete(X_train, rand_int4, 0)
 	Y_train = np.delete(Y_train, rand_int4, 0)
-
+    
 '''
 
 Visualizing each tY_trainpe of voltage signal
@@ -132,10 +132,10 @@ for i in range(len(Y_train)):
 	frequency = (1000/length)**2
 	X_train_avg_frequency = np.append(X_train_avg_frequency, [frequency])
 
-# plt.plot(X_train_fft)
-# plt.show()
-plt.scatter(Y_train, X_train_fft)
+plt.scatter(X_train_fft, X_train_feat_density)
 plt.show()
+# plt.plot(X_train_feat_density)
+# plt.show()
 
 X_test_avg_frequency = np.array([])
 X_test_fft = np.array([])
@@ -326,6 +326,52 @@ for i, clf in enumerate((svc, lin_svc, rbf_svc, poly_svc)):
  
 plt.show()
 
+C = 1.0
+svc = svm.SVC(kernel = 'linear', C=1.0).fit(X_train_feat_density_and_X_train_fft, Y_train)
+lin_svc = svm.LinearSVC(C=1.0).fit(X_train_feat_density_and_X_train_fft, Y_train)
+rbf_svc = svm.SVC(kernel = 'rbf', gamma = 0.7, C=1.0).fit(X_train_feat_density_and_X_train_fft, Y_train)
+poly_svc = svm.SVC(kernel = 'poly', degree = 3, C = 1.0).fit(X_train_feat_density_and_X_train_fft, Y_train)
+
+
+h = .02  # step size in the mesh
+ 
+# create a mesh to plot in
+
+X_train_min, X_train_max = X_train_feat_density.min() - 1, X_train_feat_density.max() + 1
+Y_train_min, Y_train_max = X_train_fft.min() - 1, X_train_fft.max() + 1
+X_train, yy = np.meshgrid(np.arange(X_train_min, X_train_max, h),
+	                     np.arange(Y_train_min, Y_train_max, h))
+# title for the plots
+titles = ['SVC with linear kernel',
+	   'LinearSVC (linear kernel)',
+	    'SVC with RBF kernel',
+	    'SVC with polynomial (degree 3) kernel']
+ 
+ 
+for i, clf in enumerate((svc, lin_svc, rbf_svc, poly_svc)):
+	 # Plot the decision boundarY_train. For that, we will assign a color to each
+	 # point in the mesh [X_train_min, X_train_max]X_train[Y_train_min, Y_train_max].
+	 plt.subplot(2, 2, i + 1)
+	 plt.subplots_adjust(wspace=0.4, hspace=0.4)
+ 
+	 Z = clf.predict(np.c_[X_train.ravel(), yy.ravel()])
+ 
+	 # Put the result into a color plot
+	 Z = Z.reshape(X_train.shape)
+	 plt.contourf(X_train, yy, Z, cmap=plt.cm.coolwarm, alpha=0.8)
+ 
+	 # Plot also the training points
+	 plt.scatter(X_test_feat_density, X_test_fft, c=Y_test, cmap=plt.cm.coolwarm)
+	 plt.xlabel('X_test_feat_density')
+	 plt.ylabel('X_test_fft')
+	 plt.xlim(X_train.min(), X_train.max())
+	 plt.ylim(yy.min(), yy.max())
+	 plt.xticks(())
+	 plt.yticks(())
+	 plt.title(titles[i])
+ 
+plt.show()
+
 # '''
 # SVM#2 X_train_feat_density and pts_diff_avg
 # '''
@@ -436,7 +482,7 @@ SVM # 4
 # print(np.shape(X_test))
 # print(np.shape(Y_test))
 
-Y_pred = lin_svc.predict(X_test_feat_density_and_X_test_fft)
+Y_pred = svc.predict(X_test_feat_density_and_X_test_fft)
 
 print("Accuracy", metrics.accuracy_score(Y_test, Y_pred))
 '''
